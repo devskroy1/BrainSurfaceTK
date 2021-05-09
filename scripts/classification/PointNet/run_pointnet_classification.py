f0b0618411e2109c0b6bdae77dee993803700715
sr4617 <sukant.roy17@imperial.ac.uk>
2021-05-09 17:09:36 +0100

    Changes LR

--- scripts/classification/PointNet/run_pointnet_classification.py
+++ scripts/classification/PointNet/run_pointnet_classification.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
 
     # 1. Model Parameters
     ################################################
-    lr = 0.1
+    lr = 0.01
     batch_size = 32
     gamma = 0.9875
     scheduler_step_size = 2
