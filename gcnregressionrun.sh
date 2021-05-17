2660a4abff0dc2439d94b8f15f2966af3ebb25c0
sr4617 <sukant.roy17@imperial.ac.uk>
2021-05-17 18:22:53 +0100

    Fixes basic saliency scores method by evaluating model on first graph

--- gcnregressionrun.sh
+++ gcnregressionrun.sh
@@ -11,11 +11,12 @@ TERM=vt100
 uptime
 
 #python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_fsavg32k/reduced_50/vtk/white False some --batch_size 64 --save_path ../tmp_save --results ./results
-#python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native_04152020/merged/reducedto_10k/pial/vtk False some --batch_size 2 --save_path ../tmp_save --results ./results
+#python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native_04152020/merged/reducedto_10k/pial/vtk False some --batch_size 64 --save_path ../tmp_save --results ./results
+python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native_04152020/merged/reducedto_10k/white/vtk False some --batch_size 64 --save_path ../tmp_save --results ./results
 #python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_fsavg32k/reduced_90/vtk/white False some --batch_size 64 --save_path ../tmp_save --results ./results
 #python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesWhite False some --batch_size 64 --save_path ../tmp_save --results ./results
 #python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesPial False some --batch_size 64 --save_path ../tmp_save --results ./results
 #python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesWhiteLeft False some --batch_size 64 --save_path ../tmp_save --results ./results
 #python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesWhiteRight False some --batch_size 64 --save_path ../tmp_save --results ./results
 #python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesPialLeft False some --batch_size 64 --save_path ../tmp_save --results ./results
-python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesPialRight False some --batch_size 64 --save_path ../tmp_save --results ./results
+#python -u models/gNNs/basicgcntrain.py /vol/bitbucket/sr4617/ForkedBrainSurfaceTK/alignedSurfacesPialRight False some --batch_size 64 --save_path ../tmp_save --results ./results
