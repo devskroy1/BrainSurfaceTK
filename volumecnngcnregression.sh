3fc387254a4970297c56cff97c7caffe11ab007c
sr4617 <sukant.roy17@imperial.ac.uk>
2021-05-17 23:08:51 +0100

    Training basic sal scores for pial

---
+++ volumecnngcnregression.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sr4617
+#SBATCH --output="volumecnnregression-%j.out"
+export PATH=/vol/bitbucket/sr4617/ForkedBrainSurfaceTK/venv/bin:$PATH
+source activate
+source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
+TERM=vt100
+/usr/bin/nvidia-smi
+uptime
+
+python3 -m scripts.regression.run_volume3d_gcn_regression
