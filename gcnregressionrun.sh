da462a3a2864976070d0b24f8632e2f4cb86217e
sr4617 <sukant.roy17@imperial.ac.uk>
2021-05-19 13:37:17 +0100

    Adds debugging prints

--- gcnregressionrun.sh
+++ gcnregressionrun.sh
@@ -5,9 +5,9 @@
 #SBATCH --output="gcntrain-%j.out"
 export PATH=/vol/bitbucket/sr4617/ForkedBrainSurfaceTK/venv/bin:$PATH
 source activate
-source /vol/cuda/10.1.105-cudnn7.6.5.32/setup.sh
+source /vol/cuda/11.0.3-cudnn8.0.5.39/setup.sh
 TERM=vt100
 /usr/bin/nvidia-smi
 uptime
 
-python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native_04152020/merged/reducedto_10k/white/vtk False all --batch_size 64 --save_path ../tmp_save --results ./results
+python -u models/gNNs/basicgcntrain.py /vol/biomedic/users/aa16914/shared/data/dhcp_neonatal_brain/surface_native_04152020/merged/reducedto_10k/white/vtk False some --batch_size 2 --save_path ../tmp_save --results ./results
