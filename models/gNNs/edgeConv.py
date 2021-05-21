57caa6b41ba8474303964e309e9fbd65c430520f
sr4617 <sukant.roy17@imperial.ac.uk>
2021-05-22 00:40:29 +0100

    Changes to edgeConv

--- models/gNNs/edgeConv.py
+++ models/gNNs/edgeConv.py
@@ -71,12 +71,12 @@ class EdgeConvGCNSegmentation(nn.Module):
 
         #EdgeConv
         # print("Before calling knn() 1")
-        #edge_index = self.knn(hidden, hidden, 20)
+        edge_index = self.knn(hidden, hidden, 20)
         # print("After calling knn1")
-        #hidden = edgeConv1(hidden.to(self.device), edge_index.to(self.device))
+        hidden = edgeConv1(hidden.to(self.device), edge_index.to(self.device))
 
         #DynEdgeConv
-        hidden = dynEdgeConv1(hidden.to(self.device))
+        #hidden = dynEdgeConv1(hidden.to(self.device))
 
         # print("hidden shape after edgeConv1")
         # print(hidden.shape)
@@ -86,12 +86,12 @@ class EdgeConvGCNSegmentation(nn.Module):
 
         # # EdgeConv
         # print("Before calling knn() 2")
-        #edge_index = self.knn(hidden, hidden, 20)
+        edge_index = self.knn(hidden, hidden, 20)
         # print("After calling knn() 2")
-        #hidden = edgeConv2(hidden.to(self.device), edge_index.to(self.device))
+        hidden = edgeConv2(hidden.to(self.device), edge_index.to(self.device))
 
         # DynEdgeConv
-        hidden = dynEdgeConv2(hidden.to(self.device))
+        #hidden = dynEdgeConv2(hidden.to(self.device))
 
         # print("hidden shape after edgeConv2")
         # print(hidden.shape)
@@ -113,8 +113,8 @@ class EdgeConvGCNSegmentation(nn.Module):
             # mlp_dims = [dims[0] * 2] + dims[1::]
             # mlp_dims = dims
             #mlp_dims = [256, 256, 256]
-            #layers.append(EdgeConv(nn=MLP(dims), aggr='max'))
-            layers.append(DynamicEdgeConv(nn=MLP(dims), k=20, aggr='max'))
+            layers.append(EdgeConv(nn=MLP(dims), aggr='max'))
+            #layers.append(DynamicEdgeConv(nn=MLP(dims), k=20, aggr='max'))
         return nn.Sequential(*layers)
 
     # def knn(self, x, k):
