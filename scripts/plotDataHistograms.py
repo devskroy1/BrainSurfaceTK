171e14173bab7a3ca308d80a2a1fe88adab6999d
sr4617 <sukant.roy17@imperial.ac.uk>
2021-05-29 02:56:46 +0100

    Plots scan age histogram for training data

--- scripts/plotDataHistograms.py
+++ scripts/plotDataHistograms.py
@@ -32,6 +32,10 @@ PATH_TO_POINTNET = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'models',
 #
 #     return data
 
+def split_patient_idx(patient_idx):
+    patient_id, session_id = patient_idx.split('_')
+    return patient_id, session_id
+
 def read_meta(path=path):
     '''Correctly reads a .tsv file into a numpy array'''
     df = pd.read_csv(path, sep='\t', header=0)
@@ -60,7 +64,7 @@ if __name__ == '__main__':
     categories = {'gender': 2, 'birth_age': 3, 'weight': 4, 'scan_age': 6, 'scan_num': 7}
     list_attrs = ['gender', 'scan_age', 'birth_age']
 
-    attribute = 'gender'
+    attribute = 'scan_age'
 
     with open(PATH_TO_POINTNET + 'src/names.pk', 'rb') as f:
         indices = pickle.load(f)
@@ -85,22 +89,36 @@ if __name__ == '__main__':
     # plt.hist(attribute_vals, bins=bins)
     #
     # plt.title("Histogram for {}".format(attribute))
+    indices_train = indices['Train']
+    #print(indices_train)
+    # pat_sess_ids_idxs = list(map(split_patient_idx, indices_train))
+    # print(pat_sess_ids_idxs)
+
+    meta_df["patient_idx"] = meta_df["participant_id"] + "_" + meta_df["session_id"].astype(str)
+    print("meta_df")
+    print(meta_df)
+    meta_df_train = meta_df[meta_df.patient_idx.isin(indices_train)]
+    print("meta_df_train")
+    print(meta_df_train)
     x_vals_scan_age = np.arange(22.5, 47.5, 2.5).tolist()
     y_vals_gender = np.arange(0, 500, 50).tolist()
     image_fp = hist_path_root + attribute + ".png"
     # plt.savefig(image_fp)
-    column = meta_df[attribute]
-    # ax = column.plot.hist(bins=25)
-    # ax.set_xlabel("Scan age")
-    # ax.set_xticks(x_vals)
+    column = meta_df_train[attribute]
+    print("column")
+    print(column)
+    ax = column.plot.hist(bins=25)
+    ax.set_xlabel("Training Scan age (weeks)")
+    ax.set_xticks(x_vals_scan_age)
 
     # column = column.astype(float)
-    ax = column.value_counts().plot(kind='bar', rot=0)
+    #ax = column.value_counts().plot(kind='bar', rot=0)
     #ax = column.plot.hist(x=attribute)
-    ax.set_xlabel("Gender")
-    ax.set_ylabel("Frequency")
-    ax.set_yticks(y_vals_gender)
-    # ax.grid('on', axis='x')
+    # ax.set_xlabel("Gender")
+    # ax.set_ylabel("Frequency")
+    # ax.set_yticks(y_vals_gender)
+
+    ax.grid('on', axis='x')
     ax.grid('on', axis='y')
     fig = ax.get_figure()
     fig.savefig(image_fp)
